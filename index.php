de021f6121f1071b23f79cef4998fbb3175938e6
Renato Martins <91312588+DryGut@users.noreply.github.com>
2022-03-30 13:38:31 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolecentes';
+$categorias[] = 'adultos';
+//print_r($categorias);
+
+$nome = 'Renato';
+$idade = 37;
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >= 6 && $idade <= 12)
+{
+  for($i == 0; $i <= count($categorias); $i++)
+    {
+      if($categorias[$i] == 'infantil')
+        echo "O Atleta ' .$nome. ' inscrito na categoria " .$categorias[$i];
+    }
+}
+else if($idade >= 13 && $idade <= 17)
+{
+  for($i == 0; $i <= count($categorias); $i++)
+    {
+      if($categorias[$i] == 'adolecentes')
+        echo "O Atleta ' .$nome. ' esta inscrito na categoria " .$categorias[$i];
+    }
+}
+else
+{
+  for($i == 0; $i <= count($categorias); $i++)
+    {
+      if($categorias == 'adultos')
+        echo "O Atleta ' .$nome. ' esta inscrito a categoria " .$categorias[$i];
+    }
+}
+?>
