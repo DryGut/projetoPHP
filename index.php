c8d31263a7900e51619c33902b48f363906d9c0b
Renato Martins <91312588+DryGut@users.noreply.github.com>
2022-03-30 15:37:24 +0000

    Initial commit

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if($idade >= 6 && $idade <= 12)
   for($i == 0; $i <= count($categorias); $i++)
     {
       if($categorias[$i] == 'infantil')
-        echo "O Atleta ' .$nome. ' inscrito na categoria " .$categorias[$i];
+        echo "O Atleta  $nome  inscrito na categoria " .$categorias[$i];
     }
 }
 else if($idade >= 13 && $idade <= 17)
@@ -23,15 +23,15 @@ else if($idade >= 13 && $idade <= 17)
   for($i == 0; $i <= count($categorias); $i++)
     {
       if($categorias[$i] == 'adolecentes')
-        echo "O Atleta ' .$nome. ' esta inscrito na categoria " .$categorias[$i];
+        echo "O Atleta  $nome  esta inscrito na categoria " .$categorias[$i];
     }
 }
 else
 {
   for($i == 0; $i <= count($categorias); $i++)
     {
-      if($categorias == 'adultos')
-        echo "O Atleta ' .$nome. ' esta inscrito a categoria " .$categorias[$i];
+      if($categorias[$i] == 'adultos')
+        echo "O Atleta  $nome  esta inscrito a categoria " .$categorias[$i];
     }
 }
 ?>
